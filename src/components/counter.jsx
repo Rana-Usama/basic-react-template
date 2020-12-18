00cc2c393b9a9f154d19a0a81c2e449c2785d4b4
unknown <ranausamauet3430@gmail.com>
2020-12-19 02:48:50 +0500

    multiple components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count:0,
-        //imageUrl:'https://picsum.photos/200'
+        value:this.props.value,
         tags:["tag1","tag2","tag3"]
     };
 
@@ -13,33 +12,39 @@ class Counter extends Component {
     };
 
     handleIncrement=()=>{
-        this.setState({count : this.state.count + 1})
+        this.setState({value : this.state.value + 1})
     }
 
     render() { 
+      
     return (
           <div>
-              {/* <img src={this.state.imageUrl} alt=""></img> */}
+              
               <span style={this.styles} className={this.getBachClasses()}>{this.formatCount()}
               </span>                  
               <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">
                 increment
+              </button>
+              <button  onClick={()=> (this.props.onDelete(this.props.id))}   
+                       className="btn btn-danger btn-sm m-2">
+                Delete
               </button>
               <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
          </div>                                                                                        
          //badge and badge-primary are bootstrat classes        
          //if only one attribute is to be style we can use inline css e.g inside span 
     );                                                                                                 
-  }     
+  }    
+  
 
     getBachClasses() {
         let classes = "badge  m-2 ";
-        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+        classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
         return classes;
     }
 
   formatCount(){
-    const{count}=this.state; 
+    const{value: count}=this.state; 
     return count=== 0 ? 'zero' :count;  
     //it can also be done like this this.state.count===0 ? 'zero' : this state.count
     //but it is cleaner way using object destructring
